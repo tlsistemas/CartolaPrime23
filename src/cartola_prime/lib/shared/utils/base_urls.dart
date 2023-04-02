aeb6246852ce6bb53e1a7c2b4fa94cfe6e387c08
Thiago Mateus <thiago.mateus@tlsistemas.com.br>
2023-04-02 15:41:53 -0300

    pegando cookie apos login

--- src/cartola_prime/lib/shared/utils/base_urls.dart
+++ src/cartola_prime/lib/shared/utils/base_urls.dart
@@ -6,5 +6,6 @@ mixin baseUrls {
   String partidas = '$_baseUrl/partidas';
   String maisEscaldos = '$_baseUrl/mercado/destaques';
   String statusMercado = '$_baseUrl/mercado/status';
-  String login = 'https://login.globo.com/api/authentication';
+  String login =
+      'https://login.globo.com/login/438?url=https://cartolafc.globo.com';
 }
