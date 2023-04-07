9e0270c8119a07ab049d51006291037acd2ffa24
Thiago Mateus <thiago.mateus@tlsistemas.com.br>
2023-04-07 11:49:27 -0300

    verificar se o time esta logado

--- src/cartola_prime/lib/viewmodel/time_vm.dart
+++ src/cartola_prime/lib/viewmodel/time_vm.dart
@@ -1,4 +1,5 @@
 import 'package:cartola_prime/models/time_logado.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 import '../data/db_cartola.dart';
 import '../repositories/clube_repo.dart';
@@ -6,6 +7,11 @@ import '../services/time_service.dart';
 
 class TimeViewModel {
   final _service = TimeService();
+  final FlutterSecureStorage _storage = const FlutterSecureStorage(
+    aOptions: AndroidOptions(
+      encryptedSharedPreferences: true,
+    ),
+  );
 
   final _repClube = ClubeRepository(DBCartola());
   TimeViewModel();
@@ -13,5 +19,13 @@ class TimeViewModel {
   Future<TimeLogado> getTimeLogado() async {
     var time = await _service.getTimeLogado();
     return time;
+  }
+
+  Future<bool> isLogado() async {
+    try {
+      return await _storage.containsKey(key: "glbid");
+    } catch (e) {
+      return false;
+    }
   }
 }
