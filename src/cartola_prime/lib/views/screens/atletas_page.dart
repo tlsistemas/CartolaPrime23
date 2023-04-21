7b3bd92aa9b08b75cc5258ef649b858ca6223a84
Thiago Mateus <thiago.mateus@tlsistemas.com.br>
2023-04-21 11:38:45 -0300

    Ajustes grid atletas

--- src/cartola_prime/lib/views/screens/atletas_page.dart
+++ src/cartola_prime/lib/views/screens/atletas_page.dart
@@ -63,125 +63,123 @@ class _AtletasPage extends State<AtletasPage> {
           return const Center(child: CircularProgressIndicator());
         } else {
           var item = snapshot.data;
-          return Column(
-            children: [
-              StaggeredGrid.count(crossAxisCount: 1, children: <Widget>[
-                _buildTile(
-                  Padding(
-                    padding: const EdgeInsets.all(3.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisSize: MainAxisSize.max,
-                      children: <Widget>[
-                        item!.time!.urlEscudoPng!.isEmpty
-                            ? const Image(
-                                height: 50,
-                                image: AssetImage('assets/images/iconp.png'))
-                            : Image.network(
-                                item.time!.urlEscudoPng!,
-                                height: 50,
-                                width: 50,
-                                alignment: Alignment.center,
-                                centerSlice: Rect.largest,
-                              ),
-                        const SizedBox(
-                          width: 20.0,
-                          height: 0.0,
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          mainAxisSize: MainAxisSize.max,
-                          children: <Widget>[
-                            Text(
-                              item.time!.nome ?? "",
-                              style: const TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold),
-                              textAlign: TextAlign.start,
+          return StaggeredGrid.count(
+            crossAxisCount: 1,
+            children: <Widget>[
+              _buildTile(
+                Padding(
+                  padding: const EdgeInsets.all(3.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.max,
+                    children: <Widget>[
+                      item!.time!.urlEscudoPng!.isEmpty
+                          ? const Image(
+                              height: 50,
+                              image: AssetImage('assets/images/iconp.png'))
+                          : Image.network(
+                              item.time!.urlEscudoPng!,
+                              height: 50,
+                              alignment: Alignment.center,
+                              centerSlice: Rect.largest,
                             ),
-                            Container(
-                              padding: const EdgeInsets.fromLTRB(0, 0, 0, 10),
-                              child: Row(
+                      const SizedBox(
+                        width: 20.0,
+                        height: 0.0,
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        mainAxisSize: MainAxisSize.max,
+                        children: <Widget>[
+                          Text(
+                            item.time!.nome ?? "",
+                            style: const TextStyle(
+                                color: Colors.black,
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold),
+                            textAlign: TextAlign.start,
+                          ),
+                          Container(
+                            padding: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+                            child: Row(
+                              children: [
+                                Text("${item.time!.esquema?.texto} ",
+                                    style: const TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 12,
+                                        fontWeight: FontWeight.bold)),
+                                Text(" ${item.time!.nome!}",
+                                    style: const TextStyle(
+                                        color: Colors.black, fontSize: 12))
+                              ],
+                            ),
+                          ),
+                          Row(
+                            children: [
+                              Column(
                                 children: [
-                                  Text("${item.time!.esquema?.texto} ",
-                                      style: const TextStyle(
-                                          color: Colors.black,
-                                          fontSize: 12,
-                                          fontWeight: FontWeight.bold)),
-                                  Text(" ${item.time!.nome!}",
-                                      style: const TextStyle(
-                                          color: Colors.black, fontSize: 12))
+                                  Text(
+                                    NumberFormat.decimalPatternDigits(
+                                            decimalDigits: 2)
+                                        .format(item.patrimonio ?? 0),
+                                    style: const TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  const SizedBox(
+                                    width: 100.0,
+                                    height: 0.0,
+                                  ),
+                                  const Text(
+                                    "Patrimônio C\$",
+                                    style: TextStyle(
+                                        color: Colors.black, fontSize: 10),
+                                  ),
                                 ],
                               ),
-                            ),
-                            Row(
-                              children: [
-                                Column(
-                                  children: [
-                                    Text(
-                                      NumberFormat.decimalPatternDigits(
-                                              decimalDigits: 2)
-                                          .format(item.patrimonio ?? 0),
-                                      style: const TextStyle(
-                                          fontSize: 16,
-                                          fontWeight: FontWeight.bold),
-                                    ),
-                                    const SizedBox(
-                                      width: 100.0,
-                                      height: 0.0,
-                                    ),
-                                    const Text(
-                                      "Patrimônio C\$",
-                                      style: TextStyle(
-                                          color: Colors.black, fontSize: 10),
-                                    ),
-                                  ],
-                                ),
-                                Column(
-                                  children: [
-                                    Text(
-                                      NumberFormat.decimalPatternDigits(
-                                              decimalDigits: 2)
-                                          .format(item.pontosCampeonato ?? 0),
-                                      style: const TextStyle(
-                                          fontSize: 16,
-                                          fontWeight: FontWeight.bold),
-                                      textAlign: TextAlign.right,
-                                    ),
-                                    const SizedBox(
-                                      width: 100.0,
-                                      height: 0.0,
-                                    ),
-                                    const Text(
-                                      "Pontos CA",
-                                      style: TextStyle(
-                                          color: Colors.black, fontSize: 10),
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            )
-                          ],
+                              Column(
+                                children: [
+                                  Text(
+                                    NumberFormat.decimalPatternDigits(
+                                            decimalDigits: 2)
+                                        .format(item.pontosCampeonato ?? 0),
+                                    style: const TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.bold),
+                                    textAlign: TextAlign.right,
+                                  ),
+                                  const SizedBox(
+                                    width: 100.0,
+                                    height: 0.0,
+                                  ),
+                                  const Text(
+                                    "Pontos CA",
+                                    style: TextStyle(
+                                        color: Colors.black, fontSize: 10),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                      Expanded(
+                        child: Text(
+                          NumberFormat.decimalPatternDigits(decimalDigits: 2)
+                              .format(item.pontos ?? 0.00),
+                          style: const TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
+                          textAlign: TextAlign.end,
                         ),
-                        Expanded(
-                          child: Text(
-                            NumberFormat.decimalPatternDigits(decimalDigits: 2)
-                                .format(item.pontos ?? 0.00),
-                            style: const TextStyle(
-                                fontSize: 18, fontWeight: FontWeight.bold),
-                            textAlign: TextAlign.end,
-                          ),
-                        )
-                      ],
-                    ),
+                      )
+                    ],
                   ),
-                  onTap: () {},
-                )
-              ]),
-              Container(
+                ),
+                onTap: () {},
+              ),
+              SizedBox(
                 height: height,
                 child: ListView.builder(
                   itemCount: item.atletas!.length,
